474095392e54f877853b14e6d298edfa838f9ca4
neeraj-dixit <neeraj.dixit@nutanix.com>
2019-01-06 18:29:07 +0530

    Added rate limiter

--- src/gateway.js
+++ src/gateway.js
@@ -4,11 +4,22 @@
 
 import express from 'express';
 import httpProxy from 'express-http-proxy';
+import rateLimit from 'express-rate-limit';
 import logger from './logger';
 import config from './config';
 
 const { gatewayconfig, apiconfig } = config;
 const nodeGateway = express();
+
+const apiRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+// apply to all requests
+// Note : If you need for a specific route add it as a middleware
+// Note : For redis store use rate-limit-redis
+nodeGateway.use(apiRateLimiter);
 
 // Code to manage handle APi's
 for (const [api, mappings] of Object.entries(apiconfig)) {
